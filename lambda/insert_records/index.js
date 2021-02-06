fafc2570cb5dbe36c47a30ff7e9f8d4efa62c930
Diezaztek <paco.tcast@gmail.com>
2021-02-06 13:03:28 -0600

    specify columns to be updated

--- lambda/insert_records/index.js
+++ lambda/insert_records/index.js
@@ -10,12 +10,12 @@ const config = {
 
 exports.handler = function(event, context, callback) {
 
-  var email = event.body.email
-  var ticketsLeft = event.body.ticketsLeft
-  var contact = event.body.contact
-  var campus = event.body.campus
+  var email = event.email
+  var ticketsLeft = event.ticketsLeft
+  var contact = event.contact
+  var campus = event.campus
 
-  var query = `INSERT INTO student
+  var query = `INSERT INTO student (email, tickets_left, contact, campus)
               VALUES('${email}', ${ticketsLeft}, '${contact}', ${campus})
               ON CONFLICT (email)
               DO
